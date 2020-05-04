2311708b2d4ca8e1495e2354bd00d6b22bf2bdc8
= <aayushdongre4@gmail.com>
2020-05-05 00:52:42 +0530

    Authentication repository

---
+++ lib/authentication/authRepository.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'dart:core';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthRepository {
+  final FirebaseAuth _firebaseAuth;
+  final GoogleSignIn _googleSignIn;
+
+  AuthRepository({FirebaseAuth firebaseAuth, GoogleSignIn googleSignIn})
+      : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
+        _googleSignIn = googleSignIn ?? GoogleSignIn();
+
+  Future<FirebaseUser> signInWithGoogle() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    await _firebaseAuth.signInWithCredential(credential);
+    return _firebaseAuth.currentUser();
+  }
+
+  Future<void> signInWithCredentials(String email, String password) async {
+    return _firebaseAuth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> logout() async {
+    return _firebaseAuth.signOut();
+  }
+
+  Future<bool> isSignIn() async {
+    final user = await _firebaseAuth.currentUser();
+    return user != null;
+  }
+
+  Future<FirebaseUser> getCurrentUser() async {
+    final FirebaseUser  currentUser = await _firebaseAuth.currentUser();
+    return currentUser;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
