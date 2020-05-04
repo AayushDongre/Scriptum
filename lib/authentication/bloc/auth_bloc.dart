351d1e09cfe8e9a4b407143c25a5c41ad87318be
= <aayushdongre4@gmail.com>
2020-05-05 01:20:48 +0530

    auth bloc events

--- lib/authentication/bloc/auth_bloc.dart
+++ lib/authentication/bloc/auth_bloc.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/rendering.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
--- lib/authentication/bloc/auth_event.dart
+++ lib/authentication/bloc/auth_event.dart
@@ -1,5 +1,21 @@
 part of 'auth_bloc.dart';
 
-abstract class AuthEvent extends Equatable {
-  const AuthEvent();
+abstract class AuthEvent {
+  const AuthEvent([List props = const []]);
+}
+
+
+class AppStarted extends AuthEvent{
+  @override
+  String toString() => 'App Started';
+}
+
+class LoggedIn extends AuthEvent{
+  @override
+  String toString() => 'User Logged In';
+}
+
+class LoggedOut extends AuthEvent{
+  @override
+  String toString() => 'User Logged out';
 }
