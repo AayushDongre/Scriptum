9a2ab22a29aa4f3bc40cd8b2e4937a971c004211
= <aayushdongre4@gmail.com>
2020-05-08 18:48:44 +0530

    tag based query

--- lib/database/dbRepository.dart
+++ lib/database/dbRepository.dart
@@ -2,14 +2,17 @@ import 'dart:async';
 import 'dart:core';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:scriptum/models/note.dart';
 import 'package:scriptum/models/user.dart';
 
 class DBRepository {
   final Firestore _firestore;
+  final User user;
 
-  DBRepository({Firestore firestore})
-      : _firestore = firestore ?? Firestore.instance;
+  DBRepository({Firestore firestore, @required this.user})
+      : assert(user != null),
+        _firestore = firestore ?? Firestore.instance;
 
   Future<void> initialiseUser(User user) async {
     return await _firestore.collection('users').document(user.uid).setData({
@@ -19,18 +22,32 @@ class DBRepository {
     });
   }
 
-  Future<Map<String, dynamic>> getUserDetails(User user) async {
+  Future<Map<String, dynamic>> getUserDetails() async {
     DocumentSnapshot snapshot =
         await _firestore.collection('users').document(user.uid).get();
     return snapshot.data;
   }
 
-  Future<void> uploadNoteData(User user, Note note) async {
+  Future<void> uploadNoteData(Note note) async {
     await _firestore
         .collection('users')
         .document(user.uid)
         .collection('notes')
         .document(note.id)
         .setData(note.tomap());
+  }
+
+  Future getFromTags(String tag) async {
+    QuerySnapshot qSnapshot = await _firestore
+        .collectionGroup('notes')
+        .where('tags', arrayContains: tag)
+        .getDocuments();
+    List<Map<String, dynamic>> notes; 
+
+    qSnapshot.documents.forEach((DocumentSnapshot element) {
+      notes.add(element.data);
+    });
+
+    return notes;
   }
 }
